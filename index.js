40d7f7d88ecda8bd4fd11af40d83c080c47422e4
Hegedüs Levente <levi@statzup.com>
2018-02-07 22:15:28 +0100

    version 0.1.0 first version of mobile view dropdown menu update every submenu updated new etc menu, just for container of stuffs forum menu deleted

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ function pauseAudio(param) {
     document.getElementById(param.id).pause();
 }
 
-function openNav() {
-    document.getElementById("header").style.height = "240px";
-    document.body.style.backgroundColor = "rgba(255,255,0,1)";
-    document.getElementsByClassName("closebtn")[0].style.opacity = "1";
-    document.getElementsByClassName("closebtn")[0].style.display = "block";
-    document.getElementsByClassName("openbtn")[0].style.color = "#f0f";
-}
-
-function closeNav() {
-    document.getElementById("header").style.height = "40px";
-    document.body.style.backgroundColor = "white";
-    document.getElementsByClassName("closebtn")[0].style.opacity = "0";
-    document.getElementsByClassName("closebtn")[0].style.display = "none";
-    document.getElementsByClassName("openbtn")[0].style.color = "#ff0";
+function toggleNav() {
+    if(document.getElementById("header").style.height === "250px") {
+      document.getElementById("header").style.height = "40px";
+      document.getElementById("header").style.transition = "all ease 0.5s";
+    }
+    else {
+      document.getElementById("header").style.height = "250px";
+      document.getElementById("header").style.transition = "all ease 0.5s";
+    }
 }
