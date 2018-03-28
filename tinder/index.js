862cb0d363ac6ce5fc97d4dad834d89126b1261b
Hegedüs Levente <levi@statzup.com>
2018-03-28 09:57:29 +0200

    fixes

---
+++ tinder/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  $(".tinder-card-container").on("swiperight", swipeRight);
+  $(".tinder-card-container").on("swipeleft", swipeLeft);
+  $('#heart').on("click", swipeRightHeart);
+  $('#heart2').on("click", swipeRightHeart);
+  $('#stop').on("click", swipeLeftStop);
+  $('#stop2').on("click", swipeLeftStop);
+});
+
+function swipeLeft(){
+  $(this).addClass('rotate-right').delay(700).fadeOut(1);
+  $('.tinder-card-container').find('.status').remove();
+  $(this).append('<div class="status dislike">Kád!</div>');
+
+  if ($(this).is(':last-child')) {
+    $('.tinder-card-container:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(500);
+  } else {
+    $(this).next().removeClass('rotate-left rotate-right').fadeIn(500);
+  }
+}
+
+function swipeRight(){
+  $(this).addClass('rotate-left').delay(500).fadeOut(1);
+  $('.tinder-card-container').find('.status').remove();
+
+  $(this).append('<div class="status like">Koala!</div>');
+  if ($(this).is(':last-child')) {
+    $('.tinder-card-container:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(500);
+  } else {
+    $(this).next().removeClass('rotate-left rotate-right').fadeIn(500);
+  }
+}
+
+
+function swipeRightHeart(){
+  let that = $('.tinder-card-container');
+  $(that).addClass('rotate-left').delay(500).fadeOut(1);
+  $('.tinder-card-container').find('.status').remove();
+
+  $(that).append('<div class="status like">Koala!</div>');
+  if ($(that).is(':last-child')) {
+    $('.tinder-card-container:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(500);
+    $('.status.like').remove();
+  } else {
+    $(that).next().removeClass('rotate-left rotate-right').fadeIn(500);
+    $('.status.like').remove();
+  }
+}
+
+function swipeLeftStop(){
+  let that = $('.tinder-card-container');
+  $(that).addClass('rotate-right').delay(700).fadeOut(1);
+  $('.tinder-card-container').find('.status').remove();
+  $(that).append('<div class="status dislike">Kád!</div>');
+
+  if ($(that).is(':last-child')) {
+    $('.tinder-card-container:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(500);
+    $('.status.dislike').remove();
+  } else {
+    $(that).next().removeClass('rotate-left rotate-right').fadeIn(500);
+    $('.status.dislike').remove();
+  }
+}
