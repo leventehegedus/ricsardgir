a4376e36ceb6af84dea3af9237834bee15a753ec
Hegedüs Levente <levi@statzup.com>
2018-04-01 16:10:06 +0200

    2049 photos updated

--- 2049/js/application.js
+++ 2049/js/application.js
@@ -15,18 +15,7 @@ window.requestAnimationFrame(function () {
         "../img/212/doge-shake-space-212.gif",
         "../img/212/doge-sunglasses-212.gif",
         "../img/212/doge-shake-212.gif",
-        "../img/212/doge-wink-212.gif",
-        "../img/114/doge-derp-114.gif",
-        "../img/114/doge-fat-114.gif",
-        "../img/114/doge-gradient-114.gif",
-        "../img/114/doge-hat-114.gif",
-        "../img/114/doge-peepers-114.gif",
-        "../img/114/doge-prizza-114.gif",
-        "../img/114/doge-rainbow-114.gif",
-        "../img/114/doge-shake-space-114.gif",
-        "../img/114/doge-sunglasses-114.gif",
-        "../img/114/doge-shake-114.gif",
-        "../img/114/doge-wink-114.gif",
+        "../img/212/2.png"
     ];
     for(var i = 0; i < imageList.length; i++ )
     {
